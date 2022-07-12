ea2b0eff0c8d6a42e98cf62f05dcae8c5200277c
Bill <bfritz@uw.edu>
2022-07-12 10:11:05 -0700

    Initial

---
+++ js/index.js
@@ -0,0 +1,75 @@
+function clicked(){
+    var tgt;
+    tgt = document.getElementById(this.id);
+    if (tgt.className == 'sidemenu__item-active'){
+        tgt = tgt.id + "_content";
+        document.getElementById(this.id).classList.remove('sidemenu__item-active')
+        document.getElementById(this.id).classList.add('sidemenu__item')
+        document.getElementById(tgt).style.display = "none"
+    } else {
+        tgt = tgt.id + "_content";
+        document.getElementById(this.id).classList.remove('sidemenu__item')
+        document.getElementById(this.id).classList.add('sidemenu__item-active')
+        document.getElementById(tgt).style.display = "block"
+    }
+}
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['DA-331', 'SDX-32', 'MX-A39', 'NN-4X3', 'DF-38Z', 'DD-121'],
+        datasets: [{
+            label: 'Object Size',
+            data: [207, 181, 292, 464, 323, 199],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+// Bar chart
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+    data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        datasets: [
+            {
+                label: "Population (millions)",
+                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                data: [2478, 5267, 734, 784, 433]
+            }
+        ]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050'
+        }
+    }
+});
